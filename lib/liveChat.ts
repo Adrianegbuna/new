4a15ba101ccfdc53d15049c5b7f837de5ea5bdde
Adrian Egbuna <ad123eg456@gmail.com>
2026-07-01 11:16:21 +0100

    Delete lib directory

--- lib/liveChat.ts
+++
@@ -1,6 +0,0 @@
-export const OPEN_LIVE_CHAT_EVENT = 'rz:open-live-chat';
-
-export function openLiveChatPopup() {
-  if (typeof window === 'undefined') return;
-  window.dispatchEvent(new Event(OPEN_LIVE_CHAT_EVENT));
-}
